1e5ac6d5e9d03ac311976f2ca428893eb9088b60
KRISHNA SAI ALLU <150152859+krsna1206@users.noreply.github.com>
2026-01-17 06:36:18 +0530

    pushed YAML files to CI/CD branch

--- ci-cd/deploy-staging.bat
+++
@@ -1,7 +0,0 @@
-@echo off 
-echo === STAGING DEPLOYMENT START === 
-pip install -r requirements.txt 
-echo Starting Flask app in staging... 
-start /B python src\app\main.py 
-echo === STAGING DEPLOYMENT SUCCESS === 
-timeout /t 3 
